edd4ad342ef7091fafe17889f406bedb998a0af9
EmelyanenkoK <Emelyanenko.Kirill@gmail.com>
2022-01-26 20:16:26 +0300

    Add mocks

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 
 (cd ./validator-controller ; ./build)
 func -SPA common/stdlib.fc common/nonstdlib.fc common/config_utils.fc proxy.fc -o build/proxy-code.fif
+func -SPA common/stdlib.fc common/nonstdlib.fc mocks/config_utils.fc proxy.fc -o build/mocked-proxy-code.fif
