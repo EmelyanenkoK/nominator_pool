347f907cd74fabf1b5d2b4414fbeb89d8c64953a
EmelyanenkoK <Emelyanenko.Kirill@gmail.com>
2022-01-27 10:51:34 +0300

    Add mocked elector

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 (cd ./validator-controller ; ./build)
 func -SPA common/stdlib.fc common/nonstdlib.fc common/config_utils.fc proxy.fc -o build/proxy-code.fif
 func -SPA common/stdlib.fc common/nonstdlib.fc mocks/config_utils.fc proxy.fc -o build/mocked-proxy-code.fif
+func -SPA common/stdlib.fc mocks/mocked-elector.fc -o build/mocked-elector-code.fif
