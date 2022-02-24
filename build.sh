b35544a5fda8abe95d598beedd566b17ebd8c0d8
awesome-doge <awesomedogewow@gmail.com>
2022-02-24 16:19:44 +0800

    Fix compilation error in apple m1.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 
 (cd ./validator-controller ; ./build)
-func -SPA common/stdlib.fc common/nonstdlib.fc common/config_utils.fc proxy.fc -o build/proxy-code.fif
-func -SPA common/stdlib.fc common/nonstdlib.fc mocks/config_utils.fc proxy.fc -o build/mocked-proxy-code.fif
-func -SPA common/stdlib.fc mocks/mocked-elector.fc -o build/mocked-elector-code.fif
+func -o build/proxy-code.fif -SPA common/stdlib.fc common/nonstdlib.fc common/config_utils.fc proxy.fc
+func -o build/mocked-proxy-code.fif -SPA common/stdlib.fc common/nonstdlib.fc mocks/config_utils.fc proxy.fc
+func -o build/mocked-elector-code.fif -SPA common/stdlib.fc mocks/mocked-elector.fc
